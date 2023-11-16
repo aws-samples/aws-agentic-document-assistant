3d3cef203d13dde19fd4e40646d341e4c7ec509b
Mohamed Ali Jamaoui <m.ali.jamaoui@gmail.com>
2023-11-16 15:25:47 +0000

    backend: Configure AssistantBackendStack app

---
+++ backend/bin/assistant-backend.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { AssistantBackendStack } from '../lib/assistant-backend-stack';
+
+const app = new cdk.App();
+
+new AssistantBackendStack(app, 'AssistantBackendStack', {
+  /* The next line to specialize this stack for the AWS Account
+   * and Region that are implied by the current CLI configuration. */
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
