714db618fc741a5023e611686d211bfd0a80083e
Mohamed Ali Jamaoui <m.ali.jamaoui@gmail.com>
2023-11-16 15:24:02 +0000

    backend: add cdk app structure

---
+++ backend/test/assistant-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AssistantBackend from '../lib/assistant-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/assistant-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AssistantBackend.AssistantBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
