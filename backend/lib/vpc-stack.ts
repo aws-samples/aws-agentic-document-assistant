f6932f455005d0a0f3e64c568910037f0e62ab99
Mohamed Ali Jamaoui <m.ali.jamaoui@gmail.com>
2023-11-16 14:43:14 +0000

    backend: Add stack to create VPC

---
+++ backend/lib/vpc-stack.ts
@@ -0,0 +1,58 @@
+// Credit goes to -> https://github.com/aws-samples/aws-genai-llm-chatbot/blob/main/lib/vpc/index.ts
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+
+export class Vpc extends Construct {
+  public readonly vpc: ec2.Vpc;
+  public readonly s3GatewayEndpoint: ec2.IGatewayVpcEndpoint;
+  public readonly s3vpcEndpoint: ec2.IGatewayVpcEndpoint;
+  public readonly dynamodbvpcEndpoint: ec2.IGatewayVpcEndpoint;
+  public readonly secretsManagerVpcEndpoint: ec2.IInterfaceVpcEndpoint;
+  public readonly apiGatewayVpcEndpoint: ec2.IInterfaceVpcEndpoint;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        {
+          name: 'isolated',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+
+    // Create a VPC endpoint for S3.
+    this.s3GatewayEndpoint = vpc.addGatewayEndpoint('S3GatewayEndpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.S3,
+    });
+
+    this.s3vpcEndpoint = vpc.addInterfaceEndpoint('S3InterfaceEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.S3,
+      open: true,
+    });
+    this.s3vpcEndpoint.node.addDependency(this.s3GatewayEndpoint);
+
+    // Create a VPC endpoint for DynamoDB.
+    this.dynamodbvpcEndpoint = vpc.addGatewayEndpoint('DynamoDBEndpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.DYNAMODB,
+    });
+
+    // Create VPC Endpoint for Secrets Manager
+    this.secretsManagerVpcEndpoint = vpc.addInterfaceEndpoint('SecretsManagerEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
+      open: true,
+    });
+
+    this.vpc = vpc;
+  }
+}
